1af3da5364ecc0c9b5c4d0a149eb0af309a0914e
“jackal7819” <jackal3166@gmail.com>
2023-02-20 02:40:48 +0200

    Create Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Featured from './components/Featured';
+import Hero from './components/Hero';
 
 const App = () => {
     return (
         <div className='App'>
             <Navbar />
+            <Hero />
             <Featured />
         </div>
     );
